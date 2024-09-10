d62179f1d2925d1efd18cf0e66acb967d0821815
unknown <vathanasroeun@gmail.com>
2024-09-10 10:49:38 +0700

    New Project was up.

---
+++ For Rothana/my-vue-app/src/App.vue
@@ -0,0 +1,8 @@
+<template >
+  <div>
+    <HeaderBar></HeaderBar>
+  </div>
+</template>
+<script setup>
+  import HeaderBar from './components/For Rothana/HeaderBar.vue';
+</script>
---
+++ For Rothana/my-vue-app/src/components/For Rothana/HeaderBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="w-full bg-emerald-600 px-20 py-3">
+        <div class="flex justify-between">
+            <!-- Left -->
+            <div class="flex justify-center items-center gap-5">
+                <!-- Logo -->
+                <div>
+                    <div class="w-20 h-20 bg-red-400 rounded-md">
+                    </div>
+                </div>
+                <!-- Bran Name -->
+                <div>
+                    <h1 class="font-bold text-xl">NIT Cambodia</h1>
+                    <p class="font-medium">Company shop | 15 brands</p>
+                </div>
+                <!-- Search Bar -->
+                <div>
+                    <div class="flex h-10 justify-center items-center">
+                        <input class="w-72 px-3 h-10 rounded-l-md border border-r-stone" type="text" name="search" placeholder="Enter for search">
+                        <!-- Logo search -->
+                        <img class="h-6 bg-white px-2 py-2 box-content rounded-r-md cursor-pointer" src="./../../assets/Img ICON/search Icon.png" alt="Search" title="Search ">
+                    </div>
+                </div>
+            </div>
+
+            <!-- Right -->
+            <div>
+                
+            </div>
+        </div>
+    </nav>
+</template>
+<script setup>
+</script>
