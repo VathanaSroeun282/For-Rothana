03bc0ad51abc37057449c3db72a1ce1b4ce80cf1
unknown <vathanasroeun@gmail.com>
2024-09-24 12:12:03 +0700

    Hello kon papp

--- For Rothana/my-vue-app/src/components/For Rothana/Body Template/CartDetail.vue
+++ For Rothana/my-vue-app/src/components/For Rothana/Body Template/CartDetail.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-48">
         <!-- cart -->
-        <div class="bg-slate-500 flex justify-center items-center py-4">
+        <div class="bg-slate-500 flex justify-center items-center pb-4 pt-6 rounded-md">
             <!-- cart layout -->
             <div>
                 <!-- cart logo -->
@@ -13,15 +13,18 @@
                     <img src="./../../../assets/Img ICON/pc1.avif" class="w-40" alt="dell pc1">
                 </div>
                 <!-- cart description -->
-                <div>
+                <div class="pt-4">
                     <!-- cart title -->
-                    <h3>Dell Vestre</h3>
+                    <div class="flex justify-between items-center">
+                        <h3 class="font-bold">Dell Vestre</h3>
+                        <button @click="taggleIcon" ><img :src="myIcon" alt="img" class="w-6"></button>
+                    </div>
                     <!-- cart model -->
                     <h4>Dis Dell Vestre</h4>
                     <!-- cart price -->
-                    <p>
+                    <p class="pb-4">
                         <span>1000$</span>
-                        <span class="text-red-600">10%</span>
+                        <span class="text-red-600 px-4">10%</span>
                         <span class="text-green-600">900$</span>
                     </p>
                     <div>
@@ -33,5 +36,12 @@
     </div>
 </template>
 <script setup>
+import { ref } from 'vue';
+import icon1 from './../../../assets/Img ICON/heart fill.png';
+import icon2 from './../../../assets/Img ICON/heart.png';
 
+    const myIcon = ref(icon1);
+    const taggleIcon = ()=>{
+        myIcon.value = myIcon.value === icon1 ? icon2 : icon1;
+    }
 </script>
